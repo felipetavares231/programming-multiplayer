1bc0cc1593287beb2cc48f0bbbcfc83f26fb5170
felipetavares231 <felipetpadua26@gmail.com>
2026-03-15 18:08:30 -0300

    adicionado todos e funcoes uteis da leetcode api

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("test");
+
+const difficulty = "EASY";
+const apiUrl = "https://alfa-leetcode-api.onrender.com";
+
+async function getProblemsList() {
+  let res = await fetch(`${apiUrl}/problems?difficulty=${difficulty}`)
+  console.log(await res.json());
+}
+
+async function getSelectedProblem() {
+  let res = await fetch(`${apiUrl}/select?titleSlug=${"two-sum"}`)
+  console.log(await res.json());
+}
+
+async function getOfficialSolution() {
+  let res = await fetch(`${apiUrl}/officialSolution?titleSlug=${"two-sum"}`)
+  console.log(await res.json());
+}
+
